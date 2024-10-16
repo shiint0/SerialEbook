3b89d4352c1d144ebc854a955f764759e5d34268
RC <shiinto@gmail.com>
2024-10-16 14:42:50 +1000

    Add handling for angle brackets

--- Pale.cs
+++ Pale.cs
@@ -21,35 +21,11 @@ namespace SerialEbook
         public Func<HtmlDocument, string> FindTitle => htmlDoc => TitleElement(htmlDoc).InnerText;
 
         protected override Func<HtmlDocument, IEnumerable<HtmlNode>> BodyElements => htmlDoc => htmlDoc.DocumentNode
-                                                                        .SelectNodes("//div[@class='entry-content']/p");
+                                                                        .SelectNodes($"//div[@class='{EntryContentClass}']/p");
 
 
         protected override Func<HtmlDocument, string> NextChapterUrl => htmlDoc => htmlDoc.DocumentNode
                                                                                 .SelectSingleNode("//span[@class='nav-next']/a")
                                                                                 ?.GetAttributeValue<string>("href", "") ?? "";
-
-        // protected override async Task BuildChapter(IChapterBuilder cb, HtmlDocument doc, int chapterIndex)
-        // {
-        //     var chapterTitlePagename = $"0-{TitleElement(doc).InnerText}";            
-        //     await cb.AddPage(chapterTitlePagename, TitleElement(doc).OuterHtml);
-
-        //     Task.WaitAll(BodyElements(doc)
-        //         .Select(async (node, index) => {
-        //             var imageSource = node.Element("img")
-        //                 ?.GetAttributeValue<string>("src", "")
-        //                 ?.Split('?')
-        //                 ?.First() ?? "";
-
-        //             if (imageSource != "")
-        //             {
-        //                 var extension = imageSource.Split('.').Last();
-        //                 await cb.AddImage($"{chapterIndex}-img-{index}.{extension}", await httpClient.GetByteArrayAsync(imageSource));
-        //             }
-        //             else
-        //                 await cb.AddPage($"{chapterIndex}-text-{index}", node.InnerHtml);
-        //         })
-        //         .ToArray()
-        //     );
-        // }
     }
 }
